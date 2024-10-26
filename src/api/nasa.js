91eb0c7018753fb3d634c4b1370a4dae666fe7f4
tenimba <tenimba.sangare@epietch.eu>
2024-10-26 18:36:13 +0200

    set search

--- src/api/nasa.js
+++ src/api/nasa.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
 export const searchImages = async (query, mediaType = "image,video") => {
-    let NASA_API_URL = "https://images-api.nasa.gov/search";
+    let NASA_API_URL = `https://images-api.nasa.gov/search?media_type=${mediaType}`;
 
     if (query) {
         NASA_API_URL += `&q=${query}`;
     }
-    if (mediaType) {
-        NASA_API_URL += `?media_type=${mediaType}`;
-    }
+    
     const response = await axios.get(NASA_API_URL);
     return response;
 };
